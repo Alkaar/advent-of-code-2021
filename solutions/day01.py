369cd6729b7a2e04b40f03b0c98424d1699c1727
Alkaar <alkaar@gmail.com>
2021-12-01 13:18:33 -0500

    Day 01 solution

--- solutions/day01.py
+++ solutions/day01.py
@@ -1,5 +1,30 @@
-file = open("resources/day01.txt", "r")
+from itertools import islice
 
-print(file.readline())
 
-file.close()
+def solution1():
+    num_increases = 0
+
+    with open("resources/day01.txt") as file:
+        prev = file.readline().rstrip()
+        while curr := file.readline().rstrip():
+            if int(curr) > int(prev):
+                num_increases = num_increases + 1
+            prev = curr
+    return num_increases
+
+
+def solution2():
+    num_increases = 0
+
+    with open("resources/day01.txt") as file:
+        window = [int(x.rstrip()) for x in list(islice(file, 3))]
+        while curr_depth := file.readline().rstrip():
+            window.append(int(curr_depth))
+            if sum(window[1:]) > sum(window[:3]):
+                num_increases = num_increases + 1
+            window.pop(0)
+    return num_increases
+
+
+print(solution1())
+print(solution2())
